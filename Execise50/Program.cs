7b6d1f7b1ca7c7728bff403f8a6880b4e8107e0a
Александр <fedoseevale@gmail.com>
2022-10-25 00:07:03 +0400

    Execise50 done

---
+++ Execise50/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // 0 - rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 1 - colomns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} | ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(4, 4, 0, 10);
+PrintArray(array2D);
+
+Console.WriteLine("Введите номер строки интересующего элемента: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца интересующего элемента: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number = 0;
+
+if ((number1 < array2D.GetLength(0)) && (number2 < array2D.GetLength(1)))
+{
+    Console.WriteLine($"{number1}, {number2} -> {number = array2D[number1, number2]}");
+}
+
+else Console.WriteLine($"{number1}, {number2} -> такого элемента в массиве нет");
