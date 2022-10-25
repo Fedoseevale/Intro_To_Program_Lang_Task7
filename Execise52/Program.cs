15f621aadc92ceca9659c4643e68f18dba9a45b7
Александр <fedoseevale@gmail.com>
2022-10-26 00:17:25 +0400

    Execise52 done

---
+++ Execise52/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+// в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // 0 - rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 1 - colomns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} | ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+double[] AverageEachColomn(int[,] matrix)
+{
+    double[] array = new double[matrix.GetLength(1)];
+    double sum = 0;
+    double average = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum = sum + matrix[i, j];
+        }
+        average = Math.Round (sum / matrix.GetLength(0), 1);
+        array[j] = average;
+        average = 0;
+        sum = 0;
+    }
+    return array;
+}
+
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}; ");
+        else Console.Write($"{array[i]}.");
+    }
+}
+
+
+int[,] array2D = CreateMatrixRndInt(5, 5, 1, 10);
+
+PrintMatrix(array2D);
+
+AverageEachColomn(array2D);
+
+double[] arr = AverageEachColomn(array2D);
+
+Console.WriteLine($"Среднее арифметическое каждого столбца: ");
+
+PrintArray(arr);
